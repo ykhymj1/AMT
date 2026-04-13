8e9313f7c4f1b1bfe482151d7f8bbd92e6efedf4
ykhymj1 <91314076+ykhymj1@users.noreply.github.com>
2026-04-13 19:59:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// AMT 알림 Service Worker v20260413-hotfix5
-const CACHE = 'amt-v20260413-hotfix5';
+// AMT 알림 Service Worker v20260413-hotfix3
+const CACHE = 'amt-v20260413-hotfix3';
 const APP_ROOT = '/AMT';
 const ASSETS = [
   APP_ROOT + '/',
